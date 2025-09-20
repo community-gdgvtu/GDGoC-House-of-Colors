765f3527f1057350e85ee3dca45bb28c53e1125b
Piyush Annigeri <piyushannigeri.vtu@gmail.com>
2025-09-20 13:26:04 +0000

    // Import the functions you need from the SDKs you need import { initial

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -11,10 +11,10 @@ const firebaseConfig = {
   apiKey: "AIzaSyA1FoKK-wAfLWT9Kbzx5ddrszHkwAKeiPY",
   authDomain: "ayurjeev-a42dc.firebaseapp.com",
   projectId: "ayurjeev-a42dc",
-  storageBucket: "ayurjeev-a42dc.appspot.com",
+  storageBucket: "ayurjeev-a42dc.firebasestorage.app",
   messagingSenderId: "72665554338",
-  appId: "1:72665554338:web:cb84c3f56817543b75e6fb",
-  measurementId: "G-KM4YYXL4V9"
+  appId: "1:72665554338:web:9ccb5df1d07e56e875e6fb",
+  measurementId: "G-SNZD6FNBPW"
 };
 
 // Initialize Firebase
